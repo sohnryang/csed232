a1ea04d847557234160a51441fb8c17745ef2ede
Ryang Sohn <loop.infinitely@gmail.com>
2023-03-10 19:53:45 +0900

    feat(pa1): complete task 2

--- assignments/pa1/task2.cpp
+++ assignments/pa1/task2.cpp
@@ -1,6 +1,30 @@
+#include <cmath>
+#include <cstring>
+#include <ios>
 #include <iostream>
 
 int main() {
-  std::cout << "[Task2] Hell world!" << std::endl;
+  char op[7]; // C style string for operator
+  double operand1, operand2, result;
+  std::cin >> op >> operand1;    // get operator and first operand
+  if (strcmp(op, "square") == 0) // check unary operator
+    result = operand1 * operand1;
+  else if (strcmp(op, "sqrt") == 0)
+    result = sqrt(operand1);
+  else {                  // case for binary operator
+    std::cin >> operand2; // get second operand
+    if (strcmp(op, "+") == 0)
+      result = operand1 + operand2;
+    else if (strcmp(op, "-") == 0)
+      result = operand1 - operand2;
+    else if (strcmp(op, "*") == 0)
+      result = operand1 + operand2;
+    else if (strcmp(op, "/"))
+      result = operand1 / operand2;
+  }
+  std::cout.precision(3); // set float output
+
+  // Use std::fixed to specify format
+  std::cout << std::fixed << result << std::endl;
   return 0;
 }
