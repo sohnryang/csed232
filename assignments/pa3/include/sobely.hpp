2c3454ccb15772a38ed51bcfc2d97e55f7f81d79
Ryang Sohn <loop.infinitely@gmail.com>
2023-04-17 21:16:59 +0900

    feat(pa3): implement sobely

--- assignments/pa3/include/sobely.hpp
+++ assignments/pa3/include/sobely.hpp
@@ -5,6 +5,9 @@
 #include <vector>
 
 class sobely : public artist {
+private:
+  std::vector<std::vector<int>> diff_y;
+
 public:
   sobely(int, int, const std::vector<int> &);
   char mapper(int, int);
--- assignments/pa3/src/sobely.cpp
+++ assignments/pa3/src/sobely.cpp
@@ -1,6 +1,18 @@
 #include "sobely.hpp"
 
 sobely::sobely(int width, int height, const std::vector<int> &image_data)
-    : artist(width, height, image_data) {}
+    : artist(width, height, image_data) {
+  for (int y = 1; y < height; y++) {
+    diff_y.push_back({});
+    for (int x = 0; x < width; x++)
+      diff_y.back().push_back(abs(image[y - 1][x] - image[y][x]));
+  }
+  diff_y.push_back({});
+  diff_y.back().assign(width, 0);
+}
 
-char sobely::mapper(int, int) {}
+char sobely::mapper(int x, int y) {
+  if (diff_y[y][x] >= 50)
+    return '-';
+  return ' ';
+}
