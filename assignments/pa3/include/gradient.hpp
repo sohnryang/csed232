ca4bfeee58149069d24036ebb966ae8412b716ad
Ryang Sohn <loop.infinitely@gmail.com>
2023-04-17 23:44:58 +0900

    feat(pa3): implement gradient

--- assignments/pa3/include/gradient.hpp
+++ assignments/pa3/include/gradient.hpp
@@ -5,6 +5,9 @@
 #include <vector>
 
 class gradient : public artist {
+private:
+  std::vector<std::vector<int>> diff_x, diff_y;
+
 public:
   gradient(int, int, const std::vector<int> &);
   char mapper(int, int);
--- assignments/pa3/src/gradient.cpp
+++ assignments/pa3/src/gradient.cpp
@@ -1,6 +1,19 @@
 #include "gradient.hpp"
+#include "sobelx.hpp"
+#include "sobely.hpp"
 
 gradient::gradient(int width, int height, const std::vector<int> &image_data)
-    : artist(width, height, image_data) {}
+    : artist(width, height, image_data) {
+  calculate_diff_x(image, diff_x);
+  calculate_diff_y(image, diff_y);
+}
 
-char gradient::mapper(int, int) {}
+char gradient::mapper(int x, int y) {
+  if (diff_x[y][x] >= 50 && diff_y[y][x] >= 50)
+    return '+';
+  else if (diff_x[y][x] >= 50)
+    return '|';
+  else if (diff_y[y][x] >= 50)
+    return '-';
+  return ' ';
+}
