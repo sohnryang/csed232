3db8293f360f147714c86c79521142cd7e69f702
Ryang Sohn <loop.infinitely@gmail.com>
2023-04-17 20:58:11 +0900

    feat(pa3): implement sobelx

--- assignments/pa3/include/sobelx.hpp
+++ assignments/pa3/include/sobelx.hpp
@@ -5,6 +5,9 @@
 #include <vector>
 
 class sobelx : public artist {
+private:
+  std::vector<std::vector<int>> diff_x;
+
 public:
   sobelx(int, int, const std::vector<int> &);
   char mapper(int, int);
--- assignments/pa3/src/sobelx.cpp
+++ assignments/pa3/src/sobelx.cpp
@@ -1,6 +1,19 @@
 #include "sobelx.hpp"
 
-sobelx::sobelx(int width, int height, const std::vector<int> &image_data)
-    : artist(width, height, image_data) {}
+#include <algorithm>
 
-char sobelx::mapper(int, int) {}
+sobelx::sobelx(int width, int height, const std::vector<int> &image_data)
+    : artist(width, height, image_data) {
+  for (auto &line : image) {
+    diff_x.push_back({});
+    for (int i = 1; i < line.size(); i++)
+      diff_x.back().push_back(abs(line[i] - line[i - 1]));
+    diff_x.back().push_back(0);
+  }
+}
+
+char sobelx::mapper(int x, int y) {
+  if (diff_x[y][x] >= 50)
+    return '|';
+  return ' ';
+}
