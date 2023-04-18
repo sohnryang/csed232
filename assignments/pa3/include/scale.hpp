56a1ae1c6f974486f44bb0882c419339857073ab
Ryang Sohn <loop.infinitely@gmail.com>
2023-04-18 13:42:49 +0900

    feat(pa3): implement scale

--- assignments/pa3/include/scale.hpp
+++ assignments/pa3/include/scale.hpp
@@ -6,6 +6,9 @@
 #include <string>
 
 class scale : public drawer {
+private:
+  int repeat_x, step_x, repeat_y, step_y;
+
 public:
   scale(artist *, int, int);
   std::string draw();
--- assignments/pa3/src/scale.cpp
+++ assignments/pa3/src/scale.cpp
@@ -1,6 +1,33 @@
 #include "scale.hpp"
 #include "artist.hpp"
 
-scale::scale(artist *artist, int, int) : drawer(artist) {}
+scale::scale(artist *art, int scale_x, int scale_y) : drawer(art) {
+  if (scale_y < 0) {
+    repeat_y = 1;
+    step_y = -scale_y;
+  } else {
+    repeat_y = scale_y;
+    step_y = 1;
+  }
+  if (scale_x < 0) {
+    repeat_x = 1;
+    step_x = -scale_x;
+  } else {
+    repeat_x = scale_x;
+    step_x = 1;
+  }
+}
 
-std::string scale::draw() {}
+std::string scale::draw() {
+  std::string res;
+  for (int y = 0; y < get_artist()->get_height(); y += step_y) {
+    std::string line;
+    for (int x = 0; x < get_artist()->get_width(); x += step_x)
+      for (int rx = 0; rx < repeat_x; rx++)
+        line += get_artist()->mapper(x, y);
+    line += '\n';
+    for (int ry = 0; ry < repeat_y; ry++)
+      res += line;
+  }
+  return res;
+}
