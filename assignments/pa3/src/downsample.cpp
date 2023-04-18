9660a98ce487c5b1a82712043bd92bc1d78039e0
Ryang Sohn <loop.infinitely@gmail.com>
2023-04-18 13:11:43 +0900

    feat(pa3): implement downsample

--- assignments/pa3/src/downsample.cpp
+++ assignments/pa3/src/downsample.cpp
@@ -2,4 +2,12 @@
 
 downsample::downsample(artist *art) : drawer(art) {}
 
-std::string downsample::draw() {}
+std::string downsample::draw() {
+  std::string res;
+  for (int y = 0; y < get_artist()->get_height(); y += 2) {
+    for (int x = 0; x < get_artist()->get_width(); x += 2)
+      res += get_artist()->mapper(x, y);
+    res += '\n';
+  }
+  return res;
+}
