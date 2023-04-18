914121250ec36cb1c2631eb438389553da3581e1
Ryang Sohn <loop.infinitely@gmail.com>
2023-04-18 19:40:07 +0900

    refactor(pa3): add more methods for testability

--- assignments/pa3/include/parser.hpp
+++ assignments/pa3/include/parser.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <istream>
+#include <memory>
 #include <string>
 #include <vector>
 
 class parser {
 public:
   std::vector<int> load_image(const char *);
+  std::vector<int> load_image(std::unique_ptr<std::istream>);
   std::vector<std::string> load_config(const char *);
+  std::vector<std::string> load_config(std::unique_ptr<std::istream>);
   void write_result(const char *, const std::string &);
 };
