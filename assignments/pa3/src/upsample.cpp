e964c4c30d99bb1fec521c762da69093de97e156
Ryang Sohn <loop.infinitely@gmail.com>
2023-04-18 13:18:18 +0900

    feat(pa3): implement upsample

--- assignments/pa3/src/upsample.cpp
+++ assignments/pa3/src/upsample.cpp
@@ -3,4 +3,17 @@
 
 upsample::upsample(artist *art) : drawer(art) {}
 
-std::string upsample::draw() {}
+std::string upsample::draw() {
+  std::string res;
+  for (int y = 0; y < get_artist()->get_height(); y++) {
+    std::string line;
+    for (int x = 0; x < get_artist()->get_width(); x++) {
+      line += get_artist()->mapper(x, y);
+      line += get_artist()->mapper(x, y);
+    }
+    line += '\n';
+    res += line;
+    res += line;
+  }
+  return res;
+}
