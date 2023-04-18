89650494a26b7b855e65f78289ab0f17d12e4559
Ryang Sohn <loop.infinitely@gmail.com>
2023-04-18 19:40:38 +0900

    test(pa3): add unit test for parser

--- assignments/pa3/tests/parser_test.cpp
+++ assignments/pa3/tests/parser_test.cpp
@@ -5,6 +5,8 @@
 
 #include <memory>
 #include <sstream>
+#include <string>
+#include <vector>
 
 TEST(Lexer, UsualInput) {
   auto st = std::make_unique<std::stringstream>("42|abcd|1234| efgh|\n");
@@ -47,4 +49,18 @@ TEST(Lexer, UnknownChar) {
   EXPECT_EQ(lex.next_token(), token("abcdefg", token_kind::WORD));
   EXPECT_EQ(lex.next_token(), token("|", token_kind::SEPARATOR));
   EXPECT_EQ(lex.next_token(), token("", token_kind::END));
+}
+
+TEST(Parser, LoadImage) {
+  auto st = std::make_unique<std::stringstream>("3|2|101|102|103|104|105|106");
+  parser p;
+  std::vector<int> image_data = p.load_image(std::move(st));
+  EXPECT_EQ(image_data, std::vector<int>({3, 2, 101, 102, 103, 104, 105, 106}));
+}
+
+TEST(Parser, LoadConfig) {
+  auto st = std::make_unique<std::stringstream>("iclassic|upsample");
+  parser p;
+  std::vector<std::string> config_data = p.load_config(std::move(st));
+  EXPECT_EQ(config_data, std::vector<std::string>({"iclassic", "upsample"}));
 }
