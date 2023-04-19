b5f9553a528ded3bfd52e1304d4c971523da58b2
Ryang Sohn <loop.infinitely@gmail.com>
2023-04-19 15:01:32 +0900

    refactor(pa3): rename arg name for consistency

--- assignments/pa3/src/classic.cpp
+++ assignments/pa3/src/classic.cpp
@@ -4,8 +4,8 @@
 
 const std::string classic::pattern = "@&%WXAHOT*^+-. ";
 
-classic::classic(int width, int height, const std::vector<int> &image)
-    : artist(width, height, image) {}
+classic::classic(int width, int height, const std::vector<int> &image_data)
+    : artist(width, height, image_data) {}
 
 char classic::mapper(int x, int y) {
   int pixel_value = pixel_at(x, y);
