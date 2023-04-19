22d643b45eb8e02b6fee82e8dfc780bc1e119a77
Ryang Sohn <loop.infinitely@gmail.com>
2023-04-19 18:14:04 +0900

    refactor(pa3): shorten the repeating code

--- assignments/pa3/src/upsample.cpp
+++ assignments/pa3/src/upsample.cpp
@@ -7,13 +7,10 @@ std::string upsample::draw() {
   std::string res;
   for (int y = 0; y < get_artist()->get_height(); y++) {
     std::string line;
-    for (int x = 0; x < get_artist()->get_width(); x++) {
-      line += get_artist()->mapper(x, y);
-      line += get_artist()->mapper(x, y);
-    }
+    for (int x = 0; x < get_artist()->get_width(); x++)
+      line += std::string(2, get_artist()->mapper(x, y));
     line += '\n';
-    res += line;
-    res += line;
+    res += line + line;
   }
   return res;
 }
