e3a93efe4e37622e4793fd1f88c7c78e5afc9f1d
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-09 17:51:05 +0900

    fix(pa4): un-format files

--- assignments/pa4/include/Image.h
+++ assignments/pa4/include/Image.h
@@ -1,100 +1,104 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
-#include "SharedPtr.h"
-#include <cassert>
-#include <cstdint>
 #include <iostream>
+#include <cstdint>
+#include <cassert>
+#include "SharedPtr.h"
 
 ///////////////////////////////////////////////////////////////////////////
 // RGB pixel type
 //    Usage:
 //      - RGB<float> rgb(0.5f, 0.1f, 0.3f);
 //        rgb[0] = 0.2f; // or equivalently, rgb.r = 0.2f;
-template <typename ValType> struct RGB {
-  union {
-    ValType data[3];
-    struct {
-      ValType r, g, b;
-    };
-  };
+template<typename ValType>
+struct RGB
+{
+	union {
+		ValType data[3];
+		struct {
+			ValType r, g, b;
+		};
+	};
 
-  RGB() {}
-  RGB(ValType r_, ValType g_, ValType b_) : r(r_), g(g_), b(b_) {}
+	RGB() {}
+	RGB(ValType r_, ValType g_, ValType b_) : r(r_), g(g_), b(b_) {}
 
-  ValType operator[](int idx) const { return data[idx]; }
-  ValType &operator[](int idx) { return data[idx]; }
+	ValType operator[](int idx) const { return data[idx]; }
+	ValType& operator[](int idx) { return data[idx]; }
 };
 
-typedef RGB<uint8_t> RGB8b;
-typedef RGB<float> RGBf;
+typedef RGB<uint8_t>	RGB8b;
+typedef RGB<float>		RGBf;
+
 
 ///////////////////////////////////////////////////////////////////////////
 // Image class template
 //
-template <typename PixelType> class Image {
+template<typename PixelType>
+class Image
+{
 private:
-  // ======= ADD CODE HERE IF NEEDED =========
-  SharedArray<PixelType> m_buff;
-  int m_width, m_height;
-
+	// ======= ADD CODE HERE IF NEEDED =========
+	SharedArray<PixelType> m_buff;
+	int m_width, m_height;
+	
 public:
-  ////////////////////////////////////////////
-  // constructors & destructor
-  ////////////////////////////////////////////
-  Image(int width, int height)
-      : m_width(width), m_height(height),
-        m_buff(new PixelType[width * height]) {}
+	////////////////////////////////////////////
+	// constructors & destructor
+	////////////////////////////////////////////
+	
+	// ======= ADD CODE HERE IF NEEDED =========
+	Image(int width, int height)
+	: m_width(width), m_height(height),
+		m_buff(new PixelType[width * height]) {}
+	Image() {}
+	Image(int width, int height, PixelType pixel)
+	: m_width(width), m_height(height),
+		m_buff(new PixelType[width * height]) {
+		for (int y = 0; y < height; y++)
+			for (int x = 0; x < width; x++)
+				m_buff[y * width + x] = pixel;
+	}
 
-  // ======= ADD CODE HERE IF NEEDED =========
-  Image() {}
-  Image(int width, int height, PixelType pixel)
-      : m_width(width), m_height(height),
-        m_buff(new PixelType[width * height]) {
-    for (int y = 0; y < height; y++)
-      for (int x = 0; x < width; x++)
-        m_buff[y * width + x] = pixel;
-  }
+	////////////////////////////////////////////
+	// assignment operator
+	////////////////////////////////////////////
+	
+	// ======= ADD CODE HERE IF NEEDED =========
+	Image &operator=(const Image &that) {
+		if (this == &that)
+			return *this;
+		m_width = that.m_width;
+		m_height = that.m_height;
+		m_buff = that.m_buff;
+		return *this;
+	}
 
-  ////////////////////////////////////////////
-  // assignment operator
-  ////////////////////////////////////////////
+	////////////////////////////////////////////
+	// element access operators
+	////////////////////////////////////////////
+	PixelType* operator[](int y) { return &m_buff[y * m_width]; }
+	const PixelType* operator[](int y) const { return &m_buff[y * m_width]; }
 
-  // ======= ADD CODE HERE IF NEEDED =========
-  Image &operator=(const Image &that) {
-    if (this == &that)
-      return *this;
-    m_width = that.m_width;
-    m_height = that.m_height;
-    m_buff = that.m_buff;
-    return *this;
-  }
-
-  ////////////////////////////////////////////
-  // element access operators
-  ////////////////////////////////////////////
-  PixelType *operator[](int y) { return &m_buff[y * m_width]; }
-  const PixelType *operator[](int y) const {
-    return static_cast<const PixelType *>(&m_buff[y * m_width]);
-  }
-
-  ////////////////////////////////////////////
-  // other methods
-  ////////////////////////////////////////////
-  //   - width(), height()
-  int width() const { return m_width; }
-  int height() const { return m_height; }
-
-  // ======= ADD CODE HERE IF NEEDED =========
+	////////////////////////////////////////////
+	// other methods
+	////////////////////////////////////////////
+	//   - width(), height()
+	int width() const { return m_width; }
+	int height() const { return m_height; }
+	
+	// ======= ADD CODE HERE IF NEEDED =========
 };
 
 // ======= ADD CODE HERE IF NEEDED =========
 
-// Miscellaneous functions
-void convert_pixel_type(const Image<RGB8b> &src, Image<RGBf> &dst);
-void convert_pixel_type(const Image<RGBf> &src, Image<RGB8b> &dst);
 
-bool load_bmp(const char *path, Image<RGB8b> &img);
-bool save_bmp(const char *path, const Image<RGB8b> &img);
+// Miscellaneous functions
+void convert_pixel_type(const Image<RGB8b>& src, Image<RGBf>& dst);
+void convert_pixel_type(const Image<RGBf>& src, Image<RGB8b>& dst);
+
+bool load_bmp(const char* path, Image<RGB8b>& img);
+bool save_bmp(const char* path, const Image<RGB8b>& img);
 
 #endif
--- assignments/pa4/include/SharedPtr.h
+++ assignments/pa4/include/SharedPtr.h
@@ -1,122 +1,126 @@
 #ifndef SHAREDPTR_H
 #define SHAREDPTR_H
 
-#include <cassert>
 #include <iostream>
+#include <cassert>
 
-template <typename ObjectType> void Deallocator(ObjectType *ptr) {
-  std::cout << "Dealloc Object" << std::endl;
-  delete ptr;
+template<typename ObjectType>
+void Deallocator(ObjectType* ptr)
+{
+	std::cout << "Dealloc Object" << std::endl;
+	delete ptr;
 }
 
-template <typename ObjectType> void ArrayDeallocator(ObjectType *ptr) {
-  std::cout << "Dealloc Array" << std::endl;
-  delete[] ptr;
+template<typename ObjectType>
+void ArrayDeallocator(ObjectType* ptr)
+{
+	std::cout << "Dealloc Array" << std::endl;
+	delete[] ptr;
 }
 
-template <typename T> using DeallocatorFuncType = void(T *);
+template<typename T>
+using DeallocatorFuncType = void (T*);
 
 // SharedPtr
-template <typename ObjectType,
-          DeallocatorFuncType<ObjectType> Dealloc = Deallocator<ObjectType>>
-class SharedPtr {
+template<typename ObjectType, DeallocatorFuncType<ObjectType> Dealloc = Deallocator<ObjectType> >
+class SharedPtr
+{
 private:
-  ObjectType *m_object;
-  int *m_ref_counter;
+	ObjectType* m_object;
+	int* m_ref_counter;
 
-  // ======= ADD CODE HERE IF NEEDED =========
-
+	// ======= ADD CODE HERE IF NEEDED =========
+				
 public:
-  ////////////////////////////////////////////
-  // Constructors & destructor
-  ////////////////////////////////////////////
-  explicit SharedPtr(ObjectType *ptr) : m_object(ptr) {
-    if (m_object == nullptr)
-      m_ref_counter = nullptr;
-    else
-      m_ref_counter = new int(1);
-  }
-  SharedPtr(const SharedPtr<ObjectType, Dealloc> &sptr)
-      : m_object(sptr.m_object), m_ref_counter(sptr.m_ref_counter) {
-    if (m_ref_counter != nullptr)
-      *m_ref_counter += 1;
-  }
-  SharedPtr() : m_object(nullptr), m_ref_counter(nullptr) {}
+	////////////////////////////////////////////
+	// Constructors & destructor
+	////////////////////////////////////////////
+	
+	// ======= ADD CODE HERE IF NEEDED =========
+	explicit SharedPtr(ObjectType *ptr) : m_object(ptr) {
+		if (m_object == nullptr)
+			m_ref_counter = nullptr;
+		else
+			m_ref_counter = new int(1);
+	}
+	SharedPtr(const SharedPtr<ObjectType, Dealloc> &sptr)
+		: m_object(sptr.m_object), m_ref_counter(sptr.m_ref_counter) {
+		if (m_ref_counter != nullptr)
+			*m_ref_counter += 1;
+	}
+	SharedPtr() : m_object(nullptr), m_ref_counter(nullptr) {}
 
-  // ======= ADD CODE HERE IF NEEDED =========
+	////////////////////////////////////////////
+	// Assignment operator
+	////////////////////////////////////////////
+	
+	// ======= ADD CODE HERE IF NEEDED =========
+	SharedPtr<ObjectType, Dealloc> &operator=(const SharedPtr<ObjectType, Dealloc> &that) {
+		if (&that == this)
+			return *this;
+		if (m_object != nullptr) {
+			*m_ref_counter -= 1;
+			if (*m_ref_counter == 0) {
+				Dealloc(m_object);
+				delete m_ref_counter;
+			}
+		}
+		m_object = that.m_object;
+		m_ref_counter = that.m_ref_counter;
+		if (m_ref_counter != nullptr)
+			*m_ref_counter += 1;
+		return *this;
+	}
+	
+	////////////////////////////////////////////
+	// Pointer operators
+	////////////////////////////////////////////
+	// operator->
+	// operator*
 
-  ////////////////////////////////////////////
-  // Assignment operator
-  ////////////////////////////////////////////
-  SharedPtr<ObjectType, Dealloc> &
-  operator=(const SharedPtr<ObjectType, Dealloc> &that) {
-    if (&that == this)
-      return *this;
-    if (m_object != nullptr) {
-      *m_ref_counter -= 1;
-      if (*m_ref_counter == 0) {
-        Dealloc(m_object);
-        delete m_ref_counter;
-      }
-    }
-    m_object = that.m_object;
-    m_ref_counter = that.m_ref_counter;
-    if (m_ref_counter != nullptr)
-      *m_ref_counter += 1;
-    return *this;
-  }
+	// ======= ADD CODE HERE IF NEEDED =========
+	ObjectType &operator*() { return *m_object; }
+	const ObjectType &operator*() const { return *m_object; }
+	ObjectType *operator->() { return m_object; }
+	const ObjectType *operator->() const { return m_object; }
+	
+	////////////////////////////////////////////
+	// Array element access operators
+	////////////////////////////////////////////
+	// operator[]
+	
+	// ======= ADD CODE HERE IF NEEDED =========
+	const ObjectType &operator[](int index) const { return m_object[index]; }
+	ObjectType &operator[](int index) { return m_object[index]; }
 
-  // ======= ADD CODE HERE IF NEEDED =========
-
-  ////////////////////////////////////////////
-  // Pointer operators
-  ////////////////////////////////////////////
-  // operator->
-  // operator*
-
-  ObjectType &operator*() { return *m_object; }
-  const ObjectType &operator*() const { return *m_object; }
-  ObjectType *operator->() { return m_object; }
-  const ObjectType *operator->() const { return m_object; }
-
-  // ======= ADD CODE HERE IF NEEDED =========
-
-  ////////////////////////////////////////////
-  // Array element access operators
-  ////////////////////////////////////////////
-  // operator[]
-
-  // ======= ADD CODE HERE IF NEEDED =========
-
-  ////////////////////////////////////////////
-  // Array element access operators
-  ////////////////////////////////////////////
-  // operator[]
-  const ObjectType &operator[](int index) const { return m_object[index]; }
-  ObjectType &operator[](int index) { return m_object[index]; }
-
-  ////////////////////////////////////////////
-  // Type casting operators
-  ////////////////////////////////////////////
-  // operator ObjectType const*() const
-  operator ObjectType const *() const {
-    return static_cast<ObjectType const *>(m_object);
-  }
-  // operator ObjectType*()
-  operator ObjectType *() const { return m_object; }
-
-  // ======= ADD CODE HERE IF NEEDED =========
-  ~SharedPtr() {
-    if (m_object == nullptr)
-      return;
-    *m_ref_counter -= 1;
-    if (*m_ref_counter == 0) {
-      Dealloc(m_object);
-      delete m_ref_counter;
-    }
-  }
+	////////////////////////////////////////////
+	// Array element access operators
+	////////////////////////////////////////////
+	// operator[]
+	
+	////////////////////////////////////////////
+	// Type casting operators
+	////////////////////////////////////////////
+	// operator ObjectType const*() const
+	// operator ObjectType*()
+	
+	// ======= ADD CODE HERE IF NEEDED =========
+	operator ObjectType const *() const {
+		return static_cast<ObjectType const *>(m_object);
+	}
+	operator ObjectType *() const { return m_object; }
+	~SharedPtr() {
+		if (m_object == nullptr)
+			return;
+		*m_ref_counter -= 1;
+		if (*m_ref_counter == 0) {
+			Dealloc(m_object);
+			delete m_ref_counter;
+		}
+	}
 };
 
-template <typename T> using SharedArray = SharedPtr<T, ArrayDeallocator<T>>;
+template<typename T>
+using SharedArray = SharedPtr<T, ArrayDeallocator<T> >;
 
 #endif
