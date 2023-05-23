222b244016da1bb14f4109f943e5c66f74dffdf4
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-24 00:12:31 +0900

    test(pa5): add tests for created block infos

--- assignments/pa5/tests/board_test.cpp
+++ assignments/pa5/tests/board_test.cpp
@@ -62,27 +62,30 @@ TEST(Board, VacantPos) {
 TEST(Board, Compactify) {
   {
     std::vector<BoardEntry> line = {{}, 1, 1, {}};
-    auto [compactified, score] = Board::reduce_line(line);
+    auto [compactified, created_blocks] = Board::reduce_line(line);
     EXPECT_THAT(compactified, ElementsAre(Eq(2)));
-    EXPECT_EQ(score, 4);
+    std::vector<std::pair<Block, int>> expected = {{2, 0}};
+    EXPECT_EQ(created_blocks, expected);
   }
   {
     std::vector<BoardEntry> line = {{}, {}};
-    auto [compactified, score] = Board::reduce_line(line);
+    auto [compactified, created_blocks] = Board::reduce_line(line);
     EXPECT_THAT(compactified, IsEmpty());
-    EXPECT_EQ(score, 0);
+    EXPECT_THAT(created_blocks, IsEmpty());
   }
   {
     std::vector<BoardEntry> line = {1, 1, 2, 2};
-    auto [compactified, score] = Board::reduce_line(line);
+    auto [compactified, created_blocks] = Board::reduce_line(line);
     EXPECT_THAT(compactified, ElementsAre(Eq(2), Eq(3)));
-    EXPECT_EQ(score, 12);
+    std::vector<std::pair<Block, int>> expected = {{2, 0}, {3, 1}};
+    EXPECT_EQ(created_blocks, expected);
   }
   {
     std::vector<BoardEntry> line = {1, 2, 2, {}};
-    auto [compactified, score] = Board::reduce_line(line);
+    auto [compactified, created_blocks] = Board::reduce_line(line);
     EXPECT_THAT(compactified, ElementsAre(Eq(1), Eq(3)));
-    EXPECT_EQ(score, 8);
+    std::vector<std::pair<Block, int>> expected = {{3, 1}};
+    EXPECT_EQ(created_blocks, expected);
   }
 }
 
@@ -132,7 +135,7 @@ TEST(Board, MoveBoard) {
   {
     Board b;
     BoardMatrix bm = b.get_board_matrix();
-    EXPECT_EQ(b.move_board(InputKind::UP), 0);
+    EXPECT_THAT(b.move_board(InputKind::UP), IsEmpty());
     EXPECT_EQ(b.get_board_matrix(), bm);
   }
   {
@@ -140,9 +143,84 @@ TEST(Board, MoveBoard) {
              {5, 6, 7, 8},
              {9, 10, 11, 12},
              {13, 14, 15, 16}});
-    EXPECT_EQ(b.move_board(InputKind::UP), 0);
-    EXPECT_EQ(b.move_board(InputKind::DOWN), 0);
-    EXPECT_EQ(b.move_board(InputKind::LEFT), 0);
-    EXPECT_EQ(b.move_board(InputKind::RIGHT), 0);
+    EXPECT_THAT(b.move_board(InputKind::UP), IsEmpty());
+    EXPECT_THAT(b.move_board(InputKind::DOWN), IsEmpty());
+    EXPECT_THAT(b.move_board(InputKind::LEFT), IsEmpty());
+    EXPECT_THAT(b.move_board(InputKind::RIGHT), IsEmpty());
+  }
+  {
+    /*
+     * 02 XX 02 XX
+     * 02 XX XX XX
+     * 02 XX 02 04
+     * 02 XX XX 04
+     */
+    Board b({BoardRow{1, {}, 1}, {1}, {1, {}, 1, 2}, {1, {}, {}, 2}});
+
+    /*
+     * 04 XX 04 08
+     * 04 XX XX XX
+     * XX XX XX XX
+     * XX XX XX XX
+     */
+    std::vector<std::pair<Block, std::pair<int, int>>> expected_created = {
+        {2, {0, 0}}, {2, {0, 2}}, {3, {0, 3}}, {2, {1, 0}}};
+    EXPECT_EQ(b.move_board(InputKind::UP), expected_created);
+    BoardMatrix expected_matrix = {BoardRow{2, {}, 2, 3}, BoardRow{2}};
+    EXPECT_EQ(b.get_board_matrix(), expected_matrix);
+
+    /*
+     * 08 08 XX XX
+     * 04 XX XX XX
+     * XX XX XX XX
+     * XX XX XX XX
+     */
+    std::vector<std::pair<Block, std::pair<int, int>>> expected_created2 = {
+        {3, {0, 0}}};
+    EXPECT_EQ(b.move_board(InputKind::LEFT), expected_created2);
+    BoardMatrix expected_matrix2 = {BoardRow{3, 3}, BoardRow{2}};
+    EXPECT_EQ(b.get_board_matrix(), expected_matrix2);
+
+    /*
+     * XX XX XX 16
+     * XX XX XX 04
+     * XX XX XX XX
+     * XX XX XX XX
+     */
+    std::vector<std::pair<Block, std::pair<int, int>>> expected_created3 = {
+        {4, {0, 3}}};
+    EXPECT_EQ(b.move_board(InputKind::RIGHT), expected_created3);
+    BoardMatrix expected_matrix3;
+    expected_matrix3[0][3] = 4;
+    expected_matrix3[1][3] = 2;
+    EXPECT_EQ(b.get_board_matrix(), expected_matrix3);
+  }
+  {
+    /*
+     * XX XX 02 XX
+     * XX XX XX XX
+     * 02 XX 02 04
+     * 02 XX XX 04
+     */
+    Board b;
+    b[{0, 2}] = 1;
+    b[{2, 0}] = 1;
+    b[{2, 2}] = 1;
+    b[{2, 3}] = 2;
+    b[{3, 0}] = 1;
+    b[{3, 3}] = 2;
+
+    /*
+     * XX XX XX XX
+     * XX XX XX XX
+     * XX XX XX XX
+     * 04 XX 04 08
+     */
+    std::vector<std::pair<Block, std::pair<int, int>>> expected_created = {
+        {2, {3, 0}}, {2, {3, 2}}, {3, {3, 3}}};
+    EXPECT_EQ(b.move_board(InputKind::DOWN), expected_created);
+    BoardMatrix expected_matrix;
+    expected_matrix[3] = {2, {}, 2, 3};
+    EXPECT_EQ(b.get_board_matrix(), expected_matrix);
   }
 }
