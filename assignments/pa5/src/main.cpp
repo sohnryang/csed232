f399021492873fa9edebf809a5943ba761dd65be
Ryang Sohn <loop.infinitely@gmail.com>
2023-05-26 18:57:17 +0900

    feat(main): add main for showing `GameUi`

--- assignments/pa5/src/main.cpp
+++ assignments/pa5/src/main.cpp
@@ -1,10 +1,10 @@
-#include "mainwindow.h"
+#include "gameui.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
-  MainWindow w;
-  w.show();
+  GameUi g;
+  g.show();
   return a.exec();
 }
